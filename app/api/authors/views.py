d3b07d7b19e784d0de4c51f5045b65695392f5f4
El96Dev <el96cpp@gmail.com>
2024-12-10 14:32:43 +0300

    Added 'put' view-function for 'authors'

--- app/api/authors/views.py
+++ app/api/authors/views.py
@@ -13,15 +13,21 @@ async def get_authors(session: AsyncSession=Depends(db_helper.scoped_session_dep
     authors = await crud.get_authors(session)
     return authors
 
+@router.post("")
+async def create_author(new_author: AuthorBase, session: AsyncSession=Depends(db_helper.scoped_session_dependency)) -> AuthorBase:
+    author = await crud.create_author(new_author, session)
+    return author
+
 @router.get("/{author_id}")
 async def get_author(author_id: int, session: AsyncSession=Depends(db_helper.scoped_session_dependency)) -> AuthorBase | None:
     author = await crud.get_author(author_id, session)
     return author
 
-@router.post("")
-async def create_author(new_author: AuthorBase, session: AsyncSession=Depends(db_helper.scoped_session_dependency)) -> AuthorBase:
-    author = await crud.create_author(new_author, session)
+@router.put("/{author_id}")
+async def update_author(author_id: int, author_update: AuthorBase, session: AsyncSession=Depends(db_helper.scoped_session_dependency)) -> AuthorBase | None:
+    author = await crud.update_author(author_id, author_update, session)
     return author
+
 
 @router.delete("/{author_id}")
 async def delete_author(author_id: int, session: AsyncSession=Depends(db_helper.scoped_session_dependency)) -> None:
